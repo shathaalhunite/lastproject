a7c3b63caee9fe6e93997969048fd22983f10580
shathaalhunite <shatha.alhnuite@gmail.com>
2021-09-08 21:21:55 +0300

    make it better

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app
+from app import create_app
 from models import db
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+migrate = Migrate(create_app, db)
+manager = Manager(create_app)
 
 
 manager.add_command('db', MigrateCommand)
