320c3248100bed3a3e43453f179b5d6680a8c8ad
shathaalhunite <shatha.alhnuite@gmail.com>
2021-09-08 22:39:54 +0300

    make it better

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 SECRET_KEY = os.urandom(32)
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Shosho11@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Shosho11@localhost/postgres'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
